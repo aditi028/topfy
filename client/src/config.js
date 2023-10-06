f0569d8d11132a8ae0c6c5c5f4b917b0d1371195
Aditi Kulkarni <AditiKulkarni2899@gmail.com>
2023-10-06 23:43:34 +0530

    ready to deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,10 @@
-// dev
-const server = "http://localhost:3001/"
-const client = "http://localhost:3000/"
+// // dev
+// const server = "http://localhost:3001/"
+// const client = "http://localhost:3000/"
 
-// //prod
-// const server = "https://topfy-server.onrender.com/"
-// const client = "https://topfysongs.netlify.app/"
+//prod
+const server = "https://topfy-server.onrender.com/"
+const client = "https://topfysongs.netlify.app/"
 
 const config = {
     URLS: {
